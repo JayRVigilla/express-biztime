d05e48d93e8753b792e96f53f86acb1621e5ee9e
Jay Vigilla <JayRVigilla@gmail.com>
2020-04-09 17:03:01 -0700

    working on sql selector for invoices/:id

--- routes/invoices.js
+++ routes/invoices.js
@@ -15,4 +15,30 @@ router.get("/", async function (req, res, next) {
     }
   })
 
+router.get('/:id', async function(req, res, next) {
+  try {
+      const invoiceRes = await db.query(
+          `SELECT *
+          FROM invoices
+          WHERE id = $1`, [req.params.id]);
+      const companiesRes = await db.query(
+          `SELECT *
+          FROM companies
+          JOIN invoices
+          ON invoice.comp_code = companies.code
+          WHERE invoice.id = $1`, [req.params.id])
+      )
+
+  if (results.rows.length === 0) {
+      throw new ExpressError(`There is no company with code of '${req.params.code}`, 404);
+    }
+    return res.json({ company: results.rows[0] });
+  } catch (err) {
+    return next(err);
+  }
+})
+
+
+
+
   module.exports = router;
